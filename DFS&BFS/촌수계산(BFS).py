659baa82d1949ed5f45ce8fb1d36791601b0296b
Your Name <qkralswgud1324@naver.com>
2021-03-03 11:49:33 +0900

    plus

--- DFS&BFS/촌수계산(BFS).py
+++ DFS&BFS/촌수계산(BFS).py
@@ -1,16 +1,18 @@
-def dfs(checked, v, data):
-    checked[v] = True
+from collections import deque
 
-    global count1
-    global count2
+def bfs(checked, start, data):
+    queue = deque()
+    queue.append(start)
+    checked[start] = True
 
-    count2 += 1
-    if v == second:
-        count1 = count2
+    while queue:
+        v = queue.popleft()
 
-    for i in data[v]:
-        if not checked[i]:
-            dfs(checked, i, data)
+        for i in data[v]:
+            if not checked[i]:
+                queue.append(i)
+                result[i] = result[v] + 1
+                checked[i] = True
 
 
 n = int(input())
@@ -18,17 +20,24 @@ first, second = map(int, input().split())
 
 m = int(input())
 data = [[] for _ in range(n + 1)]
+result = [0 for _ in range(n + 1)]
 for i in range(m):
     a, b = map(int, input().split())
     data[a].append(b)
     data[b].append(a)
 
 checked = [False] * (n + 1)
-count1 = 0
-count2 = 0
 
-dfs(checked, m, data)
-print(count1 - 1)
+bfs(checked, first, data)
+
+if result[second] != 0: print(result[second])
+else: print(-1)
+
+# 틀렸음
+'''나의 해결 : BFS는 비슷했지만 누적된 값을 가져오는 것은 실패했다.'''
+'''아이디어: 
+1. BFS 문제랑 비슷한데 누적된 값을 저장하는 배열과 방법만 알기만 하면 된다.
+'''
 
 
 '''입력
