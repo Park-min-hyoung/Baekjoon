d5d40b1e31797b1f7bb3ef30c96002b53a14c3e1
Your Name <qkralswgud1324@naver.com>
2021-03-19 11:36:00 +0900

    plus

--- Binary search/숫자 카드2.py
+++ Binary search/숫자 카드2.py
@@ -1,3 +1,5 @@
+from sys import stdin
+
 def binary_search(array, target, start, end):
     while start <= end:
         mid = (start + end) // 2
@@ -11,21 +13,29 @@ def binary_search(array, target, start, end):
 
 
 n = int(input())
-array = list(map(int, input().split()))
-array.sort()
+array = sorted(list(map(int, stdin.readline().split())))
 m = int(input())
-rq_array = list(map(int, input().split()))
+rq_array = list(map(int, stdin.readline().split()))
+
+dic = {}
+for target in array:
+    if target not in dic:
+        dic[target] = binary_search(array, target, 0, n - 1)
 
 for target in rq_array:
-    res = binary_search(array, target, 0, n)
-
-    if res == None:
-        print(0, end=' ')
+    if target in dic:
+        print(dic[target], end=' ')
     else:
-        print(res, end=' ')
+        print(0, end=' ')
 
 
-
+#틀렸음
+'''나의 해결 : 못 풀었다'''
+'''해결 아이디어
+1. 한줄 입력 받을떄는 시간 초과를 방지하기 위해 from sys import stdin을 선언하고 stdin.readline().split() 사용
+2. 이분 탐색을 하긴 하는데 dict(사전)자료형을 사용해서 dict에 key 값이 없을때만 이분탐색이 되도록 한다
+3. 이분 탐색시에 target과 일치하는 값을 찾았다면 그 때 start ~ end 사이에 target과 같은 값이 있는지 count 메소드를 통해 찾아준다
+그러므로 전체 찾아보지 않고 줄어든 범위안에서만 찾으므로 시간을 절약할 수 있다'''
 
 '''문제
 1. 상근이는 숫자 카드 N개를 가지고 있다.
